5d75bae33f2cb6af2811cca2e92a16469a8aa72a
unknown <dbzahariev@gmail.com>
2018-06-06 16:59:11 +0300

    Add new files

--- L03-FSAndMongoDB/index.js
+++ L03-FSAndMongoDB/index.js
@@ -1,9 +1,23 @@
-let fs = require('fs');
+let http = require('http')
+let fs = require('fs')
 
-// let data = fs.readFileSync('./index.js','utf8');
+http
+    .createServer((req, res) => {
+        if (req.method === 'GET') {
+            fs.readFile('./index.html', 'utf8', (err, data) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
 
-console.log(fs);
-
-// console.log(data);
-
-console.log('end');
+                res.writeHead(200, {
+                    'content-type': 'text/html'
+                })
+                res.write(data)
+                res.end()
+            })
+        }
+        else {
+            console.log('Tenx!');
+        }
+    }).listen(1234)
