6b7ba1384dde1a0da4748dbe712b9c2fe70753cf
unknown <dbzahariev@gmail.com>
2018-06-06 16:02:05 +0300

    Add new files

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,7 +1,13 @@
 let express = require('express');
-let app  = express();
+let app = express();
 const port = 1234;
 
-app.get('/home', (req,res) => res.send('Hello from Express'));
+app.get('/home', (req, res) => res.send('Hello from Express'));
 
-app.listen(port,()=>console.log(`Express running on port ${port}`));
+app.get('/home/:id:dd', (req, res) => res.send(req.params));
+
+app.all('/*', (req, res) => {
+    res.send('Hello All');
+});
+
+app.listen(port, () => console.log(`Express running on port ${port}`));
---
+++ L03-FSAndMongoDB/index.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+
+// let data = fs.readFileSync('./index.js','utf8');
+
+console.log(fs);
+
+// console.log(data);
+
+console.log('end');
