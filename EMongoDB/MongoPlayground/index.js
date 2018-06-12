6c94e2bbe14f3db48e316eafa50c757a3931406d
unknown <dbzahariev@gmail.com>
2018-06-12 18:44:15 +0300

    Add new files

--- EMongoDB/MongoPlayground/index.js
+++ EMongoDB/MongoPlayground/index.js
@@ -4,16 +4,21 @@ const qs = require('querystring')
 const port = process.env.PORT || 5000
 const handlers = require('./handlers/handlerBlender')
 
-require('./config/db')
-
-http
-  .createServer((req, res) => {
-    req.pathname = url.parse(req.url).pathname
-    req.pathquery = qs.parse(url.parse(req.url).query)
-    for (let handler of handlers) {
-      if (!handler(req, res)) {
-        break
+require('./config/db').then(() => {
+  console.log('Database redy!')
+  http
+    .createServer((req, res) => {
+      req.pathname = url.parse(req.url).pathname
+      req.pathquery = qs.parse(url.parse(req.url).query)
+      for (let handler of handlers) {
+        if (!handler(req, res)) {
+          break
+        }
       }
-    }
-  })
-  .listen(port)
+    })
+    .listen(port, () => {
+      console.log(`lisening on port ${port}`)
+    })
+}).catch(err => {
+  throw err
+})
