0ea650d70dba825164adafedb2241b461d8b93f7
dbzahariev <dbzahariev@gmail.com>
2018-07-20 12:14:34 +0300

    Add new fiels

---
+++ Angular/L02 - Components/Lab/articles-app/src/app/data/data.ts
@@ -0,0 +1,17 @@
+import { Article } from '../models/Article.model';
+import { data } from '../data/seed'
+export class ArticleData {
+    getData(): Article[] {
+        let articles:Article[] = []
+
+        for(let i =0; i<data.length;i++){
+            articles[i] = new Article(
+                data[i].title, 
+                data[i].description,
+                data[i].author,
+                data[i].imageUrl)
+        }
+
+        return articles
+    }
+}
