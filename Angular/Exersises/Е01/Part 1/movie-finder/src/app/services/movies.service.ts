063aab357122f8bd5c3b601581cefe9895c7fdbc
dbzahariev <dbzahariev@gmail.com>
2018-07-26 10:48:20 +0300

    Update movie service

--- Angular/Exersises/Е01/Part 1/movie-finder/src/app/services/movies.service.ts
+++ Angular/Exersises/Е01/Part 1/movie-finder/src/app/services/movies.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
-@Injectable({
-  providedIn: 'root'
-})
+const apiKey = '19493a1d160b6bf6c61e4d834191777c'
+
+@Injectable()
 export class MoviesService {
+  
 
   constructor(private HttpClient: HttpClient) {
     
-   }
+  }
 }
