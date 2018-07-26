4f53ed9fb61ee4b47e94acb53907fecc212f38ca
dbzahariev <dbzahariev@gmail.com>
2018-07-26 10:58:15 +0300

    Update movie service

--- Angular/Exersises/Е01/Part 1/movie-finder/src/app/services/movies.service.ts
+++ Angular/Exersises/Е01/Part 1/movie-finder/src/app/services/movies.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
-
-const apiKey = '19493a1d160b6bf6c61e4d834191777c'
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MoviesService {
-  
+  apiKey = '19493a1d160b6bf6c61e4d834191777c'
+  path: string = 'https://api.themoviedb.org/3/';
+  popular: string = 'discover/movie?sort_by=popularity.desc';
+  authentication: string = '&api_key=';
 
-  constructor(private HttpClient: HttpClient) {
-    
+  constructor(private http: HttpClient) { }
+
+  getPopular(): Observable<object>{
+    return this.http.get(this.path + this.popular + this.authentication + this.apiKey)
   }
 }
