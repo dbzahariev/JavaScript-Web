40c2c6e733aac7362480cb30488155ee0de953d1
dbzahariev <dbzahariev@gmail.com>
2018-07-26 10:41:46 +0300

    Add navigation

--- Angular/Exersises/Е01/Part 1/movie-finder/src/app/app.module.ts
+++ Angular/Exersises/Е01/Part 1/movie-finder/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
