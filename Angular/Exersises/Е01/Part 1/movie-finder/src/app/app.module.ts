89f9152ebbb8753ce3bc314265d8ab2ddbdb1e0c
dbzahariev <dbzahariev@gmail.com>
2018-07-26 10:36:37 +0300

    Add new files ok

--- Angular/Exersises/Е01/Part 1/movie-finder/src/app/app.module.ts
+++ Angular/Exersises/Е01/Part 1/movie-finder/src/app/app.module.ts
@@ -1,27 +1,18 @@
-import { MoviesService } from './services/movies.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
-import { NavigationComponent } from './navigation/navigation.component';
-
-import { HttpClient } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent,
-    NavigationComponent
+    MoviesComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClient,
-    
+    BrowserModule
   ],
-  providers: [
-    MoviesService,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
