7f0db0df182223648976801b5a9a89088b315586
dbzahariev <dbzahariev@gmail.com>
2018-07-26 14:20:52 +0300

    Add new files

--- Angular/Exersises - forms/forms/src/app/app.module.ts
+++ Angular/Exersises - forms/forms/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
