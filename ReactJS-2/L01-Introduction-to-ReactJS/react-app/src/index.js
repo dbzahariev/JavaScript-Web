a520ea15fd5217a671edd62fb833fc8820de2307
dbzahariev <dbzahariev@gmail.com>
2019-02-12 20:55:50 +0200

    Add new files

---
+++ ReactJS-2/L01-Introduction-to-ReactJS/react-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const Hello = () => <div>Hellp</div>;
+
+// ReactDOM.render(<App /> , document.getElementById('root'));
+ReactDOM.render(<Hello /> , document.getElementById('root'));
