c03c6329d4c57cd6f04dc00148139a827d52f350
dbzahariev <dbzahariev@gmail.com>
2019-02-17 17:32:03 +0200

    Add new files

--- ReactJS/Project Defense/project/src/components/common/Notification.jsx
+++ ReactJS/Project Defense/project/src/components/common/Notification.jsx
@@ -2,36 +2,35 @@ import React, { Component } from 'react'
 import observer from '../../infrastructures/Observer';
 import '../../style/notifications.css'
 
-const DEFAULT_STATE = {
-    success: null,
-    error: null,
-    loading: null,
-};
-
 export default class Notification extends Component {
     constructor(props) {
         super(props)
-        this.state = DEFAULT_STATE
+        this.state = {
+            success: null,
+            error: null,
+            loading: null,
+        }
 
-        observer.subscribe(observer.events.notification, this.showNotification)
+
+        // observer.subscribe(observer.events.notification, this.showNotification)
     }
 
     showNotification = data => {
-        let message = data.message
-        let type = data.type
-        this.setState({ [type]: message })
+        // let message = data.message
+        // let type = data.type
+        // this.setState({ type: message })
     }
 
-    hideNotification = ev => this.setState = DEFAULT_STATE
+    // hideNotification = ev => this.setState = { success: null, error: null, loading: null }
 
     render = () => {
-        if (this.state.success) {
-            return <div id="infoBox" className="alert alert-success notification"><strong>Success!</strong> {this.state.success}</div>
-        } else if (this.state.error) {
-            return <div id="errorBox" className="alert alert-danger notification"><strong>Error!</strong> {this.state.error}</div>
-        } else if (this.state.loading) {
-            return <div id="loadingBox" className="alert alert-info notification"><strong>Loading!</strong> {this.state.loading}</div>
-        }
+        // if (this.state.success) {
+        //     return <div id="infoBox" className="alert alert-success  alert-dismissible"><strong>Success!</strong> {this.state.success}</div>
+        // } else if (this.state.error) {
+        //     return <div id="errorBox" className="alert alert-danger  alert-dismissible"><strong>Error!</strong> {this.state.error}</div>
+        // } else if (this.state.loading) {
+        //     return <div id="loadingBox" className="alert alert-info  alert-dismissible"><strong>Loading!</strong> {this.state.loading}</div>
+        // }
 
         return null
     }
